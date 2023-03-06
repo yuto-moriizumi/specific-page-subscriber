4cb795d70c196069f64d38f3647e1257fb05b407
yuto.moriizumi <kurvan1112@gmail.com>
2023-03-06 17:12:16 +0900

    APIのエンドポイントを環境変数経由で指定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ const theme = createTheme({
   components: { MuiButton: { defaultProps: { variant: 'contained' } } },
 });
 
-const client = axios.create({ baseURL: 'http://localhost:3000/api' });
+const client = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL });
 
 export default function Home({
   subscriptions: defaultSubscriptions,
